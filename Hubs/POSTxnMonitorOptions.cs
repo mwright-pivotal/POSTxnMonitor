7bcd008693966e40c1d6514ceefeb3f0e54cc3b6
Mike Wright <mwright@pivotal.io>
2023-01-08 09:41:09 -0500

    app options

---
+++ Hubs/POSTxnMonitorOptions.cs
@@ -0,0 +1,8 @@
+namespace POSTxns.Hubs
+{ 
+        public class POSTxnMonitorOptions {
+            public const string POSTxnMonitorApp = "POSTxnMonitor";
+
+            public string StoreId { get; set; } = "store1";
+        }
+}
